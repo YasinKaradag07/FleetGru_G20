9e5e8223341b52b49a5ed9bf98006ec704f23b3e
YasinKaradag07 <yasinkaradag0607@gmail.com>
2022-07-19 16:35:33 +0300

    VehicleTableArrangements.feature file is created

--- src/test/java/com/fleetgru/pages/BasePage.java
+++ src/test/java/com/fleetgru/pages/BasePage.java
@@ -2,12 +2,16 @@ package com.fleetgru.pages;
 
 import com.fleetgru.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public abstract class BasePage {
 
     public BasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+
 
 
 }
