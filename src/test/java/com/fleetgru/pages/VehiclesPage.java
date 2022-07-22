82d10a89a267f7a29c2322584edfd8b93e30530d
YasinKaradag07 <yasinkaradag0607@gmail.com>
2022-07-22 21:47:02 +0300

    vehicle table arrangement step definitions and some locators implemented

--- src/test/java/com/fleetgru/pages/VehiclesPage.java
+++ src/test/java/com/fleetgru/pages/VehiclesPage.java
@@ -1,9 +1,13 @@
 package com.fleetgru.pages;
 
 import com.fleetgru.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VehiclesPage extends BasePage {
 
@@ -37,7 +41,24 @@ public class VehiclesPage extends BasePage {
     @FindBy(xpath = "(//div[@class='pagination pagination-centered']/label)[2]")
     public WebElement totalPageNumber;
 
+    @FindBy(xpath = "//button[@class='btn dropdown-toggle ']")
+    public WebElement viewPerPageDropdownButton;
 
+    @FindBy(xpath = "//a[@data-size='10']")
+    public WebElement dropdownMenuNumberTen;
 
+    @FindBy(xpath = "grid-row")
+    public WebElement vehicleTableRows;
+
+    public static int getElementsCount(By locator) {
+
+        List<WebElement> rows = Driver.getDriver().findElements(locator);
+        List<String> tableRows = new ArrayList<>();
+
+        for (WebElement row : rows) {
+            tableRows.add(row.getText());
+        }
+        return tableRows.size();
+    }
 
 }
--- src/test/java/com/fleetgru/step_definitions/VehicleTableArrangement_StepDef.java
+++ src/test/java/com/fleetgru/step_definitions/VehicleTableArrangement_StepDef.java
@@ -1,35 +1,46 @@
 package com.fleetgru.step_definitions;
 
 import com.fleetgru.pages.LoginPage;
+import com.fleetgru.pages.ManageFiltersPage;
+import com.fleetgru.pages.VehiclesPage;
+import com.fleetgru.utilities.BrowserUtils;
 import com.fleetgru.utilities.ConfigurationReader;
 import com.fleetgru.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Test;
+import org.openqa.selenium.By;
 
 public class VehicleTableArrangement_StepDef {
 
     LoginPage loginPage = new LoginPage();
+    ManageFiltersPage manageFiltersPage = new ManageFiltersPage();
+    VehiclesPage vehiclesPage = new VehiclesPage();
 
     @Given("the user logs in with valid credentials and lands on homepage")
     public void the_user_logs_in_with_valid_credentials_and_lands_on_homepage() {
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+        manageFiltersPage.userType = "Driver";
+        loginPage.loginAsUserType(manageFiltersPage.userType);
     }
 
     @When("the user navigates to Vehicles menu under Fleet menu")
     public void the_user_navigates_to_vehicles_menu_under_fleet_menu() {
-
+        vehiclesPage.waitUntilLoaderScreenDisappear();
+        manageFiltersPage.navigateToFleetVehicleModule();
+        vehiclesPage.waitUntilLoaderScreenDisappear();
     }
 
     @When("the user clicks View Per Page dropdown button")
     public void the_user_clicks_view_per_page_dropdown_button() {
-
+        BrowserUtils.sleep(3);
+        vehiclesPage.viewPerPageDropdownButton.click();
     }
 
     @When("the user selects row number options to be shown on table")
     public void the_user_selects_row_number_options_to_be_shown_on_table() {
-
+        vehiclesPage.dropdownMenuNumberTen.click();
     }
 
     @Then("the user should see the rows in table as selected number from dropdown menu")
