08d2c45615e6b914d27d18fb109416a914e709f5
alicimen <alicimen1443@gmail.com>
2022-07-23 15:56:07 +0300

    alicimen initial commit

--- src/test/java/com/fleetgru/step_definitions/VehicleTableArrangement_StepDef.java
+++ src/test/java/com/fleetgru/step_definitions/VehicleTableArrangement_StepDef.java
@@ -1,16 +1,25 @@
 package com.fleetgru.step_definitions;
 
+import com.fleetgru.pages.DashboardPage;
 import com.fleetgru.pages.LoginPage;
+import com.fleetgru.pages.VehiclesPage;
+import com.fleetgru.utilities.BrowserUtils;
 import com.fleetgru.utilities.ConfigurationReader;
 import com.fleetgru.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class VehicleTableArrangement_StepDef {
 
     LoginPage loginPage = new LoginPage();
+    VehiclesPage vehiclesPage = new VehiclesPage();
+    DashboardPage dashboardPage = new DashboardPage();
 
     @Given("the user logs in with valid credentials and lands on homepage")
     public void the_user_logs_in_with_valid_credentials_and_lands_on_homepage() {
